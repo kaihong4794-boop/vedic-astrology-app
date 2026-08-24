48f5a8b894bad02198ba852325ff8a48aa708987
kaihong4794-boop <kaihong4794@gmail.com>
2026-08-24 19:31:09 +0800

    update code

--- push.bat
+++ push.bat
@@ -7,8 +7,8 @@ echo.
 git add -A
 git commit -m "update code"
 echo.
-echo Pushing...
-git push
+echo Pushing (force, this machine's copy wins)...
+git push --force
 echo.
 echo ============================
 echo  Done. If you don't see a red "error" line above, it worked.
